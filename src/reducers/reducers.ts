194d4a25c760736af37aa3f8a3e2ef5cbc4a26b6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-18 09:04:12 +0900

    Update reducers

--- src/reducers/reducers.ts
+++ src/reducers/reducers.ts
@@ -1,18 +1,36 @@
+// tslint:disable:no-any
 import { IAction } from 'action-creators/actionCreators';
 import { actionTypes } from 'constants/actionTypes';
-import { IState } from 'state/state';
+import { IItem, IState } from 'state/state';
+
+function findItem(id: string, items: IItem[]): IItem | null {
+  for (const item of items) {
+    if (item.id === id) {
+      return item;
+    } else {
+      if (
+        item.style === 'TEXT' ||
+        item.style === 'BULLETED' ||
+        item.style === 'NUMBERED' ||
+        item.style === 'TASK' ||
+        item.style === 'TOGGLE'
+      ) {
+        return findItem(id, item.children);
+      }
+    }
+  }
+
+  return null;
+}
 
 export function reducers(state: IState, action: IAction): IState {
   const newState: IState = JSON.parse(JSON.stringify(state));
+  const payload: any = action.payload;
 
   switch (action.actionType) {
-    case actionTypes.INCREASE_COUNT: {
-      newState.count += action.payload;
-      break;
-    }
-    case actionTypes.DECREASE_COUNT: {
-      newState.count -= action.payload;
-      break;
+    case actionTypes.UPDATE_ITEM: {
+      const item: IItem | null = findItem(payload.id, newState.pages[0].items);
+      Object.assign(item, payload);
     }
     default:
   }
