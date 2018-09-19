b241f867e122a2e16c11a05fe11f98df38bb6f93
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-19 21:44:08 +0900

    Implement traverse.shiftItem

--- src/utils/traverse.ts
+++ src/utils/traverse.ts
@@ -25,6 +25,7 @@ function hasChildren(item: IItem): item is ITextItem | IBulletedItem | INumbered
 export const traverse: {
   find(items: IItem[], id: string): IItem | null;
   addItem(items: IItem[], prevId: string, newId?: string): void;
+  shiftItem(items: IItem[], id: string): void;
 } = {
   find: (items: IItem[], id: string): IItem | null => {
     for (const item of items) {
@@ -62,6 +63,33 @@ export const traverse: {
       } else {
         if (hasChildren(item)) {
           traverse.addItem(item.children, prevId, newId);
+        }
+      }
+    }
+  },
+  shiftItem(items: IItem[], id: string): void {
+    for (let i: number = 0; i < items.length; i += 1) {
+      const item: IItem = items[i];
+      if (item.id === id) {
+        const prevItem: IItem | null = items[i - 1] || null;
+        if (prevItem === null /* If prev item is nothing, ignore */) {
+          // Noop
+        } else {
+          if (hasChildren(prevItem)) {
+            items.splice(i, 1);
+            prevItem.children.push(item);
+
+            if (hasChildren(item)) {
+              prevItem.children = prevItem.children.concat(item.children);
+              item.children = [];
+            }
+          } else {
+            // Noop
+          }
+        }
+      } else {
+        if (hasChildren(item)) {
+          traverse.shiftItem(item.children, id);
         }
       }
     }
