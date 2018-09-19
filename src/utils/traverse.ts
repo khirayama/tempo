bcde2e6d56f182af2ad5bc9a3fcda0aa113dbaf4
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-19 21:23:47 +0900

    Implement traverse.addItem

--- src/utils/traverse.ts
+++ src/utils/traverse.ts
@@ -1,4 +1,4 @@
-import { IItem } from 'state/state';
+import { IBulletedItem, IItem, INumberedItem, ITaskItem, ITextItem, IToggleItem } from 'state/state';
 
 /*
  * find
@@ -11,21 +11,27 @@ import { IItem } from 'state/state';
  * deleteGroup
  * turnInto
 */
+
+function hasChildren(item: IItem): item is ITextItem | IBulletedItem | INumberedItem | ITaskItem | IToggleItem {
+  return (
+    item.style === 'TEXT' ||
+    item.style === 'BULLETED' ||
+    item.style === 'NUMBERED' ||
+    item.style === 'TASK' ||
+    item.style === 'TOGGLE'
+  );
+}
+
 export const traverse: {
   find(items: IItem[], id: string): IItem | null;
+  addItem(items: IItem[], prevId: string, newId?: string): void;
 } = {
   find: (items: IItem[], id: string): IItem | null => {
     for (const item of items) {
       if (item.id === id) {
         return item;
       } else {
-        if (
-          item.style === 'TEXT' ||
-          item.style === 'BULLETED' ||
-          item.style === 'NUMBERED' ||
-          item.style === 'TASK' ||
-          item.style === 'TOGGLE'
-        ) {
+        if (hasChildren(item)) {
           const result: IItem | null = traverse.find(item.children, id);
           if (result !== null) {
             return result;
@@ -35,5 +41,29 @@ export const traverse: {
     }
 
     return null;
+  },
+  addItem: (items: IItem[], prevId: string, newId?: string): void => {
+    for (let i: number = 0; i < items.length; i += 1) {
+      const item: IItem = items[i];
+      if (item.id === prevId) {
+        const prevItem: IItem | null = traverse.find(items, prevId);
+        const newItem: ITextItem = {
+          id: newId || '', // Add id
+          style: 'TEXT',
+          text: '',
+          children: [],
+        };
+        if (prevItem !== null && hasChildren(prevItem)) {
+          newItem.children = prevItem.children;
+          prevItem.children = [];
+        }
+
+        items.splice(i + 1, 0, newItem);
+      } else {
+        if (hasChildren(item)) {
+          traverse.addItem(item.children, prevId, newId);
+        }
+      }
+    }
   },
 };
