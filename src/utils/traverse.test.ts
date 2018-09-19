1efe3516f72ea08a4b60d81dfbda64bec30cade6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-20 07:17:33 +0900

    Add test cases for traverse.shiftGroup

--- src/utils/traverse.test.ts
+++ src/utils/traverse.test.ts
@@ -338,4 +338,72 @@ describe('traverse', () => {
       expect(items[1].id).toEqual('6');
     });
   });
+
+  describe('shiftGroup', () => {
+    it('shift group id: 1', () => {
+      const items: any = copyItems(sampleItems);
+      traverse.shiftGroup(items, '1');
+      expect(items[0].id).toEqual('1');
+      expect(items[0].children[0].id).toEqual('2');
+      expect(items[0].children[1].id).toEqual('3');
+      expect(items[0].children[1].children[0].id).toEqual('4');
+      expect(items[0].children[1].children[1].id).toEqual('5');
+      expect(items[1].id).toEqual('6');
+    });
+
+    it('shift group id: 2', () => {
+      const items: any = copyItems(sampleItems);
+      traverse.shiftGroup(items, '2');
+      expect(items[0].id).toEqual('1');
+      expect(items[0].children[0].id).toEqual('2');
+      expect(items[0].children[1].id).toEqual('3');
+      expect(items[0].children[1].children[0].id).toEqual('4');
+      expect(items[0].children[1].children[1].id).toEqual('5');
+      expect(items[1].id).toEqual('6');
+    });
+
+    it('shift group id: 3', () => {
+      const items: any = copyItems(sampleItems);
+      traverse.shiftGroup(items, '3');
+      expect(items[0].id).toEqual('1');
+      expect(items[0].children[0].id).toEqual('2');
+      expect(items[0].children[0].children[0].id).toEqual('3');
+      expect(items[0].children[0].children[0].children[0].id).toEqual('4');
+      expect(items[0].children[0].children[0].children[1].id).toEqual('5');
+      expect(items[1].id).toEqual('6');
+    });
+
+    it('shift group id: 4', () => {
+      const items: any = copyItems(sampleItems);
+      traverse.shiftGroup(items, '4');
+      expect(items[0].id).toEqual('1');
+      expect(items[0].children[0].id).toEqual('2');
+      expect(items[0].children[1].id).toEqual('3');
+      expect(items[0].children[1].children[0].id).toEqual('4');
+      expect(items[0].children[1].children[1].id).toEqual('5');
+      expect(items[1].id).toEqual('6');
+    });
+
+    it('shift group id: 5', () => {
+      const items: any = copyItems(sampleItems);
+      traverse.shiftGroup(items, '5');
+      expect(items[0].id).toEqual('1');
+      expect(items[0].children[0].id).toEqual('2');
+      expect(items[0].children[1].id).toEqual('3');
+      expect(items[0].children[1].children[0].id).toEqual('4');
+      expect(items[0].children[1].children[0].children[0].id).toEqual('5');
+      expect(items[1].id).toEqual('6');
+    });
+
+    it('shift group id: 6', () => {
+      const items: any = copyItems(sampleItems);
+      traverse.shiftGroup(items, '6');
+      expect(items[0].id).toEqual('1');
+      expect(items[0].children[0].id).toEqual('2');
+      expect(items[0].children[1].id).toEqual('3');
+      expect(items[0].children[1].children[0].id).toEqual('4');
+      expect(items[0].children[1].children[1].id).toEqual('5');
+      expect(items[0].children[2].id).toEqual('6');
+    });
+  });
 });
