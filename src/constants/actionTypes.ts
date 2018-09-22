accc0ba4750e858326a61d512afb69a2f267eb8f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-22 10:20:42 +0900

    Add action types

--- src/constants/actionTypes.ts
+++ src/constants/actionTypes.ts
@@ -1,5 +1,12 @@
 export const actionTypes: {
   [key: string]: string;
 } = {
+  ADD_ITEM: '__ADD_ITEM',
   UPDATE_ITEM: '__UPDATE_ITEM',
+  SHIFT_ITEM: '__SHIFT_ITEM',
+  UNSHIFT_ITEM: '__UNSHIFT_ITEM',
+  DELETE_ITEM: '__DELETE_ITEM',
+  CANCEL_ITEM: '__CANCEL_ITEM',
+  PREPEND_ITEM: '__PREPEND_ITEM',
+  APPEND_ITEM: '__APPEND_ITEM',
 };
