347b3634f483c28b17b4b9cbd46747358a287d53
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-22 10:20:52 +0900

    Create action creators

--- src/action-creators/actionCreators.ts
+++ src/action-creators/actionCreators.ts
@@ -10,6 +10,56 @@ export interface IAction {
 
 export type IDispatch = (action: IAction) => void;
 
+export async function addItem(dispatch: IDispatch, item: { prevId: string }): Promise<IAction> {
+  const action: IAction = {
+    actionType: actionTypes.ADD_ITEM,
+    payload: item,
+  };
+  dispatch(action);
+
+  return action;
+}
+
+export async function shiftItem(dispatch: IDispatch, item: { id: string }): Promise<IAction> {
+  const action: IAction = {
+    actionType: actionTypes.SHIFT_ITEM,
+    payload: item,
+  };
+  dispatch(action);
+
+  return action;
+}
+
+export async function unshiftItem(dispatch: IDispatch, item: { id: string }): Promise<IAction> {
+  const action: IAction = {
+    actionType: actionTypes.UNSHIFT_ITEM,
+    payload: item,
+  };
+  dispatch(action);
+
+  return action;
+}
+
+export async function deleteItem(dispatch: IDispatch, item: { id: string }): Promise<IAction> {
+  const action: IAction = {
+    actionType: actionTypes.DELETE_ITEM,
+    payload: item,
+  };
+  dispatch(action);
+
+  return action;
+}
+
+export async function cancelItem(dispatch: IDispatch, item: { id: string }): Promise<IAction> {
+  const action: IAction = {
+    actionType: actionTypes.CANCEL_ITEM,
+    payload: item,
+  };
+  dispatch(action);
+
+  return action;
+}
+
 export async function updateItem(dispatch: IDispatch, item: { id: string; text?: string }): Promise<IAction> {
   const action: IAction = {
     actionType: actionTypes.UPDATE_ITEM,
