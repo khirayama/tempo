ae291796576914219b663ff8ca76a55c92cbe896
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-27 08:27:14 +0900

    Add complicated items for test

--- src/utils/traverse.test.ts
+++ src/utils/traverse.test.ts
@@ -33,6 +33,41 @@ sampleItems2
 expect(items[0].id).toEqual('1');
 expect(items[0].children[0].id).toEqual('2');
 expect(items[0].children[0].children[0].id).toEqual('3');
+
+- - -
+
+sampleItems3
+- id: 2 TEXT
+  - id: 3 TEXT
+  - id: 4 TEXT
+- id: 28 DIVIDER
+- id: 9 HEADER
+- id: 5 TASK false
+  - id: 6 TASK false
+  - id: 7 TASK true
+  - id: 8 TASK false
+- id: 10 BULLETED
+  - id: 11 BULLETED
+  - id: 12 BULLETED
+  - id: 13 BULLETED
+- id: 14 NUMBERED
+  - id: 15 NUMBERED
+  - id: 16 NUMBERED
+  - id: 17 NUMBERED
+  - id: 31 DIVIDER
+  - id: 32 NUMBERED
+- id: 18 TOGGLE false
+  - id: 19 TOGGLE false
+    - id: 22 TEXT
+    - id: 23 TEXT
+  - id: 20 TOGGLE true
+    - id: 33 TEXT
+    - id: 34 TEXT
+  - id: 21 TOGGLE false
+    - id: 26 TEXT
+    - id: 27 TEXT
+- id: 30 QUOTE
+
 */
 
 const sampleItems: IItem[] = [
@@ -114,6 +149,199 @@ const sampleItems2: IItem[] = [
     style: 'TEXT',
     text: 'text 4',
     children: [],
+  },
+];
+
+const sampleItems3: IItem[] = [
+  {
+    id: '2',
+    style: 'TEXT',
+    text: 'Sample Text 2',
+    children: [
+      {
+        id: '3',
+        style: 'TEXT',
+        text: 'Sample Text 3',
+        children: [],
+      },
+      {
+        id: '4',
+        style: 'TEXT',
+        text: 'Sample Text 4',
+        children: [],
+      },
+    ],
+  },
+  {
+    id: '28',
+    style: 'DIVIDER',
+  },
+  {
+    id: '9',
+    style: 'HEADER',
+    text: 'TODO LIST',
+  },
+  {
+    id: '5',
+    style: 'TASK',
+    text: 'Task 1',
+    completed: false,
+    children: [
+      {
+        id: '6',
+        style: 'TASK',
+        text: 'Task 2',
+        completed: false,
+        children: [],
+      },
+      {
+        id: '7',
+        style: 'TASK',
+        text: 'Task 3',
+        completed: true,
+        children: [],
+      },
+      {
+        id: '8',
+        style: 'TASK',
+        text: 'Task 4',
+        completed: false,
+        children: [],
+      },
+    ],
+  },
+  {
+    id: '10',
+    style: 'BULLETED',
+    text: 'bulleted 1',
+    children: [
+      {
+        id: '11',
+        style: 'BULLETED',
+        text: 'bulleted 2',
+        children: [],
+      },
+      {
+        id: '12',
+        style: 'BULLETED',
+        text: 'bulleted 3',
+        children: [],
+      },
+      {
+        id: '13',
+        style: 'BULLETED',
+        text: 'bulleted 4',
+        children: [],
+      },
+    ],
+  },
+  {
+    id: '14',
+    style: 'NUMBERED',
+    text: 'numbered 1',
+    children: [
+      {
+        id: '15',
+        style: 'NUMBERED',
+        text: 'numbered 2',
+        children: [],
+      },
+      {
+        id: '16',
+        style: 'NUMBERED',
+        text: 'numbered 3',
+        children: [],
+      },
+      {
+        id: '17',
+        style: 'NUMBERED',
+        text: 'numbered 4',
+        children: [],
+      },
+      {
+        id: '31',
+        style: 'DIVIDER',
+      },
+      {
+        id: '32',
+        style: 'NUMBERED',
+        text: 'numbered 1',
+        children: [],
+      },
+    ],
+  },
+  {
+    id: '18',
+    style: 'TOGGLE',
+    text: 'toggle 1',
+    opened: false,
+    children: [
+      {
+        id: '19',
+        style: 'TOGGLE',
+        text: 'toggle 2',
+        opened: false,
+        children: [
+          {
+            id: '22',
+            style: 'TEXT',
+            text: 'toggle child 1',
+            children: [],
+          },
+          {
+            id: '23',
+            style: 'TEXT',
+            text: 'toggle child 2',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: '20',
+        style: 'TOGGLE',
+        text: 'toggle 3',
+        opened: true,
+        children: [
+          {
+            id: '33',
+            style: 'TEXT',
+            text: 'toggle child 1',
+            children: [],
+          },
+          {
+            id: '34',
+            style: 'TEXT',
+            text: 'toggle child 2',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: '21',
+        style: 'TOGGLE',
+        text: 'toggle 4',
+        opened: false,
+        children: [
+          {
+            id: '26',
+            style: 'TEXT',
+            text: 'toggle child 1',
+            children: [],
+          },
+          {
+            id: '27',
+            style: 'TEXT',
+            text: 'toggle child 2',
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '30',
+    style: 'QUOTE',
+    text: 'When in Rome, do as the Romans do.',
   },
 ];
 
