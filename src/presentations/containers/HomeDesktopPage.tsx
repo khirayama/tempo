850677b9e7e8f5d4056fb45fc40c1e6df064328f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-10-16 07:41:06 +0900

    Use new components for Home

--- src/presentations/containers/HomeDesktopPage.tsx
+++ src/presentations/containers/HomeDesktopPage.tsx
@@ -1,151 +1,23 @@
 // tslint:disable:no-any react-this-binding-issue
+import * as classNames from 'classnames';
 import * as React from 'react';
 
-import { updateItem } from 'action-creators/actionCreators';
-import { EditableText } from 'presentations/components/EditableText';
+import { Pad } from 'presentations/components/Pad';
 import { Container, IContainerProps } from 'presentations/containers/Container';
 import { Link } from 'router/Link';
-import { IItem, IPage, IState, ITextItem } from 'state/state';
+import { IItem, IPaper, IState, ITextItem, IUI } from 'state/state';
 
 export class HomeDesktopPage extends Container<{}, IState> {
-  private onClickIncrementButton: any;
-
-  private onClickDecrementButton: any;
-
   constructor(props: IContainerProps) {
     super(props);
 
     this.state = { ...this.getState() };
-
-    this.actions = {
-      updateItem: (item: { id: string; text?: string }): Promise<{}> => {
-        return updateItem(this.dispatch, item);
-      },
-    };
-  }
-
-  // tslint:disable-next-line:max-func-body-length
-  public renderItem(item: IItem): JSX.Element {
-    let children: JSX.Element[] = [];
-
-    switch (item.style) {
-      case 'TEXT': {
-        if (item.children.length) {
-          children = item.children.map(this.renderItem.bind(this));
-        }
-
-        return (
-          <div className="Item" key={item.id}>
-            <EditableText
-              value={item.text}
-              onChange={(event: React.FormEvent<HTMLInputElement>): void => {
-                const value: string = event.currentTarget.value;
-
-                this.actions.updateItem({ id: item.id, text: value });
-              }}
-            />
-            {children}
-          </div>
-        );
-      }
-
-      case 'BULLETED': {
-        if (item.children.length) {
-          children = item.children.map(this.renderItem.bind(this));
-        }
-
-        return (
-          <div className="Item" key={item.id}>
-            <div>・ {item.text}</div>
-            {children}
-          </div>
-        );
-      }
-
-      case 'NUMBERED': {
-        // FYI: Consider index increment
-        const index: number = 1;
-
-        if (item.children.length) {
-          children = item.children.map(this.renderItem.bind(this));
-        }
-
-        return (
-          <div className="Item" key={item.id}>
-            <div>
-              {index} {item.text}
-            </div>
-            {children}
-          </div>
-        );
-      }
-
-      case 'TASK': {
-        if (item.children.length) {
-          children = item.children.map(this.renderItem.bind(this));
-        }
-
-        return (
-          <div className="Item" key={item.id}>
-            <div>
-              <input type="checkbox" checked={item.completed} /> {item.text}
-            </div>
-            {children}
-          </div>
-        );
-      }
-
-      case 'TOGGLE': {
-        if (item.children.length) {
-          children = item.children.map(this.renderItem.bind(this));
-        }
-
-        return (
-          <details className="Item" key={item.id} open={item.opened}>
-            <summary>{item.text}</summary>
-            {children}
-          </details>
-        );
-      }
-
-      case 'HEADER': {
-        return (
-          <div className="Item" key={item.id}>
-            <h2>{item.text}</h2>
-          </div>
-        );
-      }
-
-      case 'QUOTE': {
-        return (
-          <blockquote className="Item" key={item.id}>
-            {item.text}
-          </blockquote>
-        );
-      }
-
-      case 'DIVIDER': {
-        return (
-          <div className="Item" key={item.id}>
-            <hr />
-          </div>
-        );
-      }
-
-      default:
-    }
-
-    return <span />;
   }
 
   public render(): JSX.Element {
-    const page: IPage = this.state.pages[0];
+    const focusedId: string | null = this.state.ui.focusedId;
+    const paper: IPaper = this.state.binders[0].papers[0];
 
-    return (
-      <section className="Page HomeDesktopPage">
-        <h1>{page.title}</h1>
-        {page.items.map(this.renderItem.bind(this))}
-      </section>
-    );
+    return <Pad paper={paper} store={this.props.store} />;
   }
 }
